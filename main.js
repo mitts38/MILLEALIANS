8655a5e681d31e7760432b688cc6bf8c933987ab
Mitali Vasisht <vamdev37@gmail.com>
2019-11-25 13:55:40 -0600

    add more exercise

--- main.js
+++ main.js
@@ -25,3 +25,29 @@ const myfavoriteFoods = (array) => {
     return newArray
 }
  console.log(myfavoriteFoods(foodArray));
+
+
+
+ // loop though the numbers in the 'listOfNumbers' array (using a for.. of loop)
+// write a conditional statement that checks whether the number is even or odd
+// if it is even print "2 is even" (plug in the current #)
+// if it is odd print "3 is even" (plug in the current #)
+let listOfNumbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9,10]
+for (const singleNum of listOfNumbers) {
+    if(singleNum % 2 === 0) {
+        console.log(`${singleNum}is even`)
+    } else
+    { console.log(`${singleNum}is odd`)
+    
+}
+    
+//Declare three variables (eyeColor, hairColor, height). If eyeColor is blue and hairColor is red and height is 6ft, console log the string template `red hair, blue eyes, and 6ft tall, otherwise console log "not a match"
+const eyeColor = "green"
+const hairColor = "purple"
+const height = "2ft"
+if (eyeColor ==="blue" && hairColor ==="red" && height ==="6ft") {
+    console.log(`${hairColor} hair, ${eyeColor} eyes, and ${height} tall`)
+} else {
+    console.log("not a match")
+}
+}
